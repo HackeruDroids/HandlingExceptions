9f47a34b16feef737e72e26fb83270fdf759748a
hackerudroids <tomerjava@gmail.com>
2017-03-09 21:37:52 +0200

    Exceptions

--- src/edu/tomer/java/Circle.java
+++ src/edu/tomer/java/Circle.java
@@ -8,19 +8,34 @@ public class Circle {
     private double radius;
 
     //constructor
-    public Circle(double radius) throws Exception {
+    public Circle(double radius)  {
         setRadius(radius);
     }
+
+
     //setter
-    public void setRadius(double radius) throws Exception {
+    public void setRadius(double radius)  {
         //circle has no (GUI)Graphical User Interface.
         // no way to show the user an alert
-        if (radius < 0)
-            throw new Exception("Invalid radius " + radius);
+        if (radius < 0) {
+            throw new RadiusException(radius + "");
+        }
+
         this.radius = radius;
     }
     //getArea
     public double getArea() {
         return Math.PI * radius * radius;
+    }
+
+
+    private class RadiusException extends RuntimeException {
+        public RadiusException() {
+
+        }
+
+        public RadiusException(String message) {
+            super(message);
+        }
     }
 }
--- src/edu/tomer/java/Main.java
+++ src/edu/tomer/java/Main.java
@@ -1,17 +1,69 @@
 package edu.tomer.java;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
 import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
-        // write your code here
 
+//        try {
+//            Circle c = new Circle(-2);
+//        }
+//        catch (Exception e){
+//            throw new RuntimeException("Crystal clear message", e);
+//        }
+
+        //getInputStream
+
+        //http://w...
+        //convert a  string to url
+
+        //Checked Exceptions - extend Exception - Must Handle or try{}catch{}
+        URLConnection con = null;
+        try {
+            URL url = new URL("https://docs.oracle.com/javase/7/docs/api/java/lang/Throwable.html");
+            //open a connection
+            con = url.openConnection();
+            InputStream in = con.getInputStream();
+            System.out.println("Yay");
+            int x = Integer.parseInt("Hello");
+            //multi-catch
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (NumberFormatException e) {
+            e.printStackTrace();
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            if (con != null) {
+
+            }
+        }
+
+    }
+
+    public static int getInt(String message) {
         Scanner scan = new Scanner(System.in);
-        System.out.println("Enter Radius: ");
-        double r = scan.nextDouble();
 
-        //let's handle the exception
-        Circle c = new Circle(r);
+        int number;
+        while (true) {
+            try {
+                System.out.println(message);
+                String input = scan.nextLine();
+                number = Integer.valueOf(input);
+                return number;
+            } catch (NumberFormatException e) {
+                //recursion
+//            return getInt("Try again!");
+                message = "Try Again";
+            }
+        }
     }
 }
